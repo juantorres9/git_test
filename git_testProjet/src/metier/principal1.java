98f12d488bc2a4cad7130a131e98a2032697d78b
torrju01 <juan.torres.zetino@gmail.com>
2016-05-27 11:16:17 +0200

    modification phrase  Master

--- git_testProjet/src/metier/principal1.java
+++ git_testProjet/src/metier/principal1.java
@@ -15,7 +15,7 @@ public class principal1 {
 		t3.start();
 		t4.start();
 		//HAUT VAUT3 ;	
-		System.out.println("Le nom du thread principal est 2 ");
+		System.out.println("BONJOUR ,Je viens du branche MASTER   ");
 		//HAUT VAUT4 ; 
 		
 	}
